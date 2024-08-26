8b982c84f891cd3ae84125bc79a6af93cabb5a0c
Lerirove <143636343+Lerirove@users.noreply.github.com>
2024-08-25 21:28:19 -0400

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const yesButton = document.getElementById('yesButton');
     const noButton = document.getElementById('noButton');
     const message = document.getElementById('message');
+    const buttonsContainer = document.querySelector('.buttons');
 
     // Al hacer clic en "Siguiente" del mensaje de bienvenida
     welcomeNextButton.addEventListener('click', () => {
@@ -21,14 +22,19 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Mostrar mensaje de agradecimiento y confeti al hacer clic en "Sí"
-    yesButton.addEventListener('click', () => {
-        message.textContent = '¡Gracias por hacerme el hombre más feliz del mundo!';
-        confetti(); // Llama al efecto de confeti al hacer clic en "Sí"
-    });
+    yesButton.addEventListener('click', showThankYouMessage);
 
     // Intercambiar posiciones de los botones "Sí" y "No" al tocar o hacer clic en "No"
     noButton.addEventListener('click', swapButtons);
     noButton.addEventListener('touchstart', swapButtons);
+
+    function showThankYouMessage() {
+        message.textContent = '¡Gracias por hacerme el hombre más feliz del mundo!';
+        confetti(); // Llama al efecto de confeti al hacer clic en "Sí"
+        
+        // Ocultar los botones después de mostrar el mensaje
+        buttonsContainer.style.display = 'none';
+    }
 
     function swapButtons() {
         // Guardar las posiciones actuales
@@ -44,6 +50,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
         noButton.style.left = `${yesButtonRect.left}px`;
         noButton.style.top = `${yesButtonRect.top}px`;
+
+        // Convertir el "No" en "Sí" y mostrar el mensaje
+        noButton.textContent = "Sí";
+        noButton.removeEventListener('click', swapButtons);
+        noButton.addEventListener('click', showThankYouMessage);
     }
 });
 
