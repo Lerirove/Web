80c7a894ccb4e7b199020fa694658f0580f143f7
Lerirove <143636343+Lerirove@users.noreply.github.com>
2024-08-25 21:08:27 -0400

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -21,18 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('yesButton').addEventListener('click', () => {
         message.textContent = '¡Gracias por hacerme el hombre más feliz del mundo!';
-        if (typeof confetti === 'function') {
-            confetti({
-                particleCount: 100,
-                spread: 70,
-                origin: { y: 0.6 }
-            });
-        } else {
-            console.error('La función de confeti no está definida.');
-        }
+        confetti(); // Llama al efecto de confeti al hacer clic en "Sí"
     });
 
-    noButton.addEventListener('mouseover', () => {
+    // Función para mover el botón "No"
+    function moveNoButton() {
         const containerRect = mainContent.getBoundingClientRect();
         const buttonRect = noButton.getBoundingClientRect();
 
@@ -48,5 +41,12 @@ document.addEventListener('DOMContentLoaded', () => {
         noButton.style.position = 'absolute';
         noButton.style.left = `${newLeft}px`;
         noButton.style.top = `${newTop}px`;
-    });
+    }
+
+    // Event listener para PC
+    noButton.addEventListener('mouseover', moveNoButton);
+
+    // Event listener para móviles
+    noButton.addEventListener('touchstart', moveNoButton);
 });
+
